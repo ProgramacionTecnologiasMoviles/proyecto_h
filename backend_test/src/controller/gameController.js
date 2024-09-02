77603d552ba2de94f2ee9087a2cd20c37a5f126f
Daniel Gonzalez <juandanielgonzalez004@gmail.com>
2024-09-01 21:24:53 -0500

    delete unused backend

--- backend_test/src/controller/gameController.js
+++
@@ -1,6 +0,0 @@
-const getGame = async (req, res) => {
-  const gameId = Math.floor(Math.random() * 9999);
-  return res.status(202).json({ data: { gameId: gameId } });
-};
-
-module.exports = { getGame };
--- backend_test/src/routes/gameRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const gameController = require("../controller/gameController");
-
-const router = express.Router();
-
-router.get("/", gameController.getGame);
-
-module.exports = router;
