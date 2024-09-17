82cbbd1c390e2a2c53ec247fa4274511ed90903e
Apricold <jhosgri@hotmail.com>
2024-09-17 12:36:41 -0500

    test front conexion

--- Frontend/hooks/useCreateGame.js
+++ Frontend/hooks/useCreateGame.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 
-const url = "http://192.168.115.42:3000";
+const url = "http://172.20.10.3:8000/api";
 const useCreateGame = async () => {
   try {
-    const response = await axios.get(`${url}/game`);
-    console.log(response.data.data.gameId);
-    return response.data.data.gameId;
+    data = { hostUser: 3, creditsbetted: 10, game: 1 };
+    const response = await axios.post(`${url}/create_match`, data);
+    console.log(response);
+    console.log(response.data.id);
+    return response.data.id;
   } catch (error) {
     console.log(error);
   }
