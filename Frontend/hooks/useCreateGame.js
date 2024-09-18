e00bda4682d3daa978f62254e81bb2ff6d7925c1
Daniel Gonzalez <juandanielgonzalez004@gmail.com>
2024-09-17 20:06:18 -0500

    PipeOffset Added

--- Frontend/hooks/useCreateGame.js
+++ Frontend/hooks/useCreateGame.js
@@ -1,12 +1,10 @@
 import axios from "axios";
 
-const url = "http://172.20.10.3:8000/api";
+const url = "http://192.168.54.42:8000/api";
 const useCreateGame = async () => {
   try {
-    data = { hostUser: 3, creditsbetted: 10, game: 1 };
+    data = { hostUser: 1, creditsbetted: 10, game: 1 };
     const response = await axios.post(`${url}/create_match`, data);
-    console.log(response);
-    console.log(response.data.id);
     return response.data.id;
   } catch (error) {
     console.log(error);
